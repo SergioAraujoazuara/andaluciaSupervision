a104da911c55ad80ddde317f9d10dc55138d7121
SergioAraujoazuara <sergio.araujo@tpfingenieria.com>
2024-02-23 12:59:50 +0100

    env

--- firebase_config.js
+++ firebase_config.js
@@ -10,17 +10,14 @@ import {getAuth} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBJi9qrrsaaaxCnyhPQ2HhxO2qSJkwaJZ4",
-    authDomain: "inspeccion-454e4.firebaseapp.com",
-    projectId: "inspeccion-454e4",
-    storageBucket: "inspeccion-454e4.appspot.com",
-    messagingSenderId: "1036900171846",
-    appId: "1:1036900171846:web:9be632ca76bcc380c40a47",
-    measurementId: "G-WBGQ7GL9LS"
-  };
-
-// Initialize Firebase
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
 
 export const app = initializeApp(firebaseConfig);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react()]
+    // Resto de tu configuración
+  };
+});
