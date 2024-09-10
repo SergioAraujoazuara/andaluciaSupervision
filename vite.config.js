3b38883f63492e4e0716bc8837cc240d32222986
SergioAraujoazuara <sergio.araujo@tpfingenieria.com>
2024-09-10 16:01:34 +0200

    deploy correcto

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react';
 import { loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-  // Cargar las variables de entorno del archivo .env según el modo
-  const env = loadEnv(mode, process.cwd());
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return {
-    plugins: [react()],
-    define: {
-      'process.env': env  // Asegúrate de definir las variables de entorno aquí
-    },
+    plugins: [react()]
+    // Resto de tu configuración
   };
 });
