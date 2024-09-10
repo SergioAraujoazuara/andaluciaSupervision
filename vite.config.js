7fc19badcb2d4d36fc9aa462437bde9d29f121cc
SergioAraujoazuara <sergio.araujo@tpfingenieria.com>
2024-09-10 15:50:02 +0200

    config actions git

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-  // Cargar variables de entorno
+  // Cargar las variables de entorno del archivo .env según el modo
   const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [react()],
     define: {
-      // Esto es opcional, pero puedes usarlo para exponer las variables de entorno a tu código frontend
-      'process.env': env,
+      'process.env': env  // Asegúrate de definir las variables de entorno aquí
     },
-    // Resto de tu configuración
   };
 });
