3cec8198b03db10cd7543688af1fa46925e96cd7
SergioAraujoazuara <sergio.araujo@tpfingenieria.com>
2024-11-04 11:36:01 +0100

    pdf pk

--- src/Components/FeatureSendMail/SendMail.jsx
+++ src/Components/FeatureSendMail/SendMail.jsx
@@ -4,15 +4,15 @@ import emailjs from 'emailjs-com';
 function SendMail() {
     const sendEmail = () => {
         emailjs.send(
-          'service_pqxgzd2',     // Coloca aquí tu Service ID
-          'template_u93shac',    // Coloca aquí tu Template ID
+          VITE_SENDEMAIL_SERVICE_ID,     // Coloca aquí tu Service ID
+          VITE_SENDEMAIL_TEMPLATE_ID,    // Coloca aquí tu Template ID
           {
             from_name: 'TPF ingenieria | App inspección',
             to_name: 'Nombre de usuario',
             to_email: 'tpfingenieriaspain@gmail.com',   // Email dinámico del destinatario
             message: 'Se ha generado un informe pdf, ya puedes consultarlo en la app',
           },
-          'KxYdy0XnlflbZLV_s'      // Coloca aquí tu Public Key
+          VITE_SENDEMAIL_PUBLIC_KEY      // Coloca aquí tu Public Key
         )
         .then((result) => {
           console.log('Correo enviado con éxito', result.status, result.text);
